0c5957eec24bfb27a2f54aac8ce7b9c0873b71af
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-09 18:01:21 +0100

    Implement plugin system
    - Moved plugins, and probability to their own files in `plugins/`
    - Dynamically import in bot.py

---
+++ plugins/hahahaha_plugin.py
@@ -0,0 +1,17 @@
+import re
+from random import randint
+from datetime import datetime
+from telethon import events, sync
+from probability_function import probability
+
+
+# Hahahahahaha
+async def haha(event):
+    sender = await event.get_sender()
+    response_length = randint(2,5)
+    response = event.pattern_match.group(1)*response_length    # Format the response (between 2 to 5 times the original length)
+    print([datetime.now().strftime("%c")], [sender.id], sender.username, [response_length], event.pattern_match.string)
+    if probability(0.2):
+        await event.reply(response.capitalize())    # Capitalize the response
+
+haha.event = events.NewMessage(pattern=re.compile(r"(hah?\w+a)$", re.I).match, outgoing=False)
---
+++ plugins/mein_fuhrer_plugin.py
@@ -0,0 +1,19 @@
+import re
+from random import randint
+from telethon import events, sync
+from probability_function import probability
+
+
+# MEIN FÜHRER!
+async def mein_fuhrer(event):
+    sender = await event.get_sender()
+    response_id = randint(0,2) # Roll for the response
+    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}[{response_id}]: {event.pattern_match.string}")
+    if response_id == 0:
+        await event.reply("Heil Hitler!")
+    elif response_id == 1:
+        await event.reply("Sieg Heil!")
+    elif response_id == 2:
+        await event.reply(file = "CAADAgADWgADraG3CP76-OQcP7msAg")    # Send a sticker
+
+mein_fuhrer.event =  events.NewMessage(pattern=re.compile(r"(hitler|führer|fuhrer)", re.I).search, outgoing=False)
