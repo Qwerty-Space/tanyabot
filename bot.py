38ca054ea16d2c8c0afafdb1405345403c9f69f8
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-09 17:54:58 +0100

    Move plugins, and dynamically import

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
-import configparser
-import logging
-from random import random, randint
 import re
+import sys
+import logging
+import configparser
+from os import listdir, path
 from datetime import datetime
-from os import path
-from urllib.parse import urljoin
+from inspect import getmembers
 
 from telethon import TelegramClient, custom, events, sync
+from telethon.tl.types import (MessageEntityTextUrl, MessageEntityUrl,
+                               MessageMediaDocument, MessageMediaPhoto)
 
 logging.basicConfig(level=logging.WARNING)
 
@@ -27,116 +29,30 @@ print("Connecting...")
 client = TelegramClient(path.join(script_dir, session_name), api_id, api_hash)
 
 
-### FUNCTIONS ###
-# Probability
-def probability(percent):
-    Probability = random() < percent
-    print(Probability)
-    return Probability
+### IMPORT PLUGINS ###
+plugindir = "plugins"
+script_dir = path.dirname(path.realpath(__file__))
+sys.path.append(path.join(script_dir, plugindir))
+pluginfiles = listdir(plugindir)
+
+for pluginfile in pluginfiles:
+    if re.search(r".+plugin\.py$", pluginfile):
+        plugin_name = f"{pluginfile}"[:-3]
+        plugin = __import__(f"{plugin_name}", globals(), locals(), [], 0)
+        for name, handler in getmembers(plugin):
+            if hasattr(handler, 'event'):
+                client.add_event_handler(handler, handler.event)
 
 
-### "PLUGINS" ###
-# /start
-@client.on(events.NewMessage(pattern=r"(/start)$"))
-async def on_start(event):
-    sender = await event.get_sender()    # Get the sender
-    if event.is_private:    # If command was sent in private
-        print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
-        await event.respond('This is a bot for silly replies')
-
-
-# /ping
-@client.on(events.NewMessage(pattern=r"(/ping)$"))
-async def ping_pong(event):
-    sender = await event.get_sender()
-    if event.is_private:
-        a = datetime.timestamp(datetime.now())
-        message = await event.reply("**Pong!**")
-        b = datetime.timestamp(datetime.now()) - a
-        print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string} [{b:.3f}]")
-        await message.edit(f"**Pong!**\nTook `{b:.3f}` seconds")
-
-
-# Hi, Hungry!
-# Check if they say "I am" "I'm" or any variant of (+ at least 1, but at most 3 words)
-@client.on(events.NewMessage(pattern=r"(?i)^(?:i'?m|i am) ((?:\S+ ?){1,3})$", outgoing=False, forwards=False))
-async def i_am_dad(event):
-    sender = await event.get_sender()
-    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
-    if probability(0.2):    # Reply 20% of the time
-        await event.reply("Hi " + event.pattern_match.group(1)) 
-
-
-# MEIN FÜHRER!
-@client.on(events.NewMessage(pattern=re.compile(r"(hitler|führer|fuhrer)", re.I).search, outgoing=False))
-async def mein_fuhrer(event):
-    sender = await event.get_sender()
-    response_id = randint(0,2) # Roll for the response
-    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}[{response_id}]: {event.pattern_match.string}")
-    if response_id == 0:
-        await event.reply("Heil Hitler!")
-    elif response_id == 1:
-        await event.reply("Sieg Heil!")
-    elif response_id == 2:
-        await event.reply(file = "CAADAgADWgADraG3CP76-OQcP7msAg")    # Send a sticker
-
-
-# Insult
-@client.on(events.NewMessage(pattern=re.compile(r"(love|fuck|screw|damn?) (you|u) Tanya", re.I).search, outgoing=False, forwards=False))
-async def insult(event):
-    sender = await event.get_sender()
-    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
-    await event.reply(event.pattern_match.group(1)+" you too!")    # "Love you too!"
-
-
-# Hahahahahaha
-@client.on(events.NewMessage(pattern=re.compile(r"(hah?\w+a)$", re.I).match, outgoing=False))
-async def haha(event):
-    sender = await event.get_sender()
-    response_length = randint(2,5)
-    response = event.pattern_match.group(1)*response_length    # Format the response (between 2 to 5 times the original length)
-    print([datetime.now().strftime("%c")], [sender.id], sender.username, [response_length], event.pattern_match.string)
-    if probability(0.2):
-        await event.reply(response.capitalize())    # Capitalize the response
-
-
-# Yes or no
-@client.on(events.NewMessage(pattern=re.compile(r"(yes|y)(/| or )(no|n)\??$").search, forwards=False))    # Matches "y/n" "yes or no" "yes/no?" etc
-async def yes_or_no(event):
-    sender = await event.get_sender()
-    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
-    if probability(0.5):
-        await event.reply("Yes.")
-    else:
-        await event.reply("No.")
-
-
-# Did you just assume their gender?!
-@client.on(events.NewMessage(pattern=re.compile(r"\b(he|his|she|her)\b").search, forwards=False))
-async def assume(event):
-    sender = await event.get_sender()
-    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
-    if probability(0.2):
-        await event.reply("Did you just assume their gender?!")
-
-
-# Subreddit
-@client.on(events.NewMessage(pattern=re.compile(r"(?:\s|^)(/)?(r/)(\w+)\b").search))
-async def link_subreddit(event):
-    sender = await event.get_sender()
-    subreddit_name = event.pattern_match.group(3)
-    subreddit_link = urljoin("https://reddit.com/r/", subreddit_name)
-    log = f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}"
-
-    async def subreddit_reply():
-        await event.reply(f"[/r/{subreddit_name}]({subreddit_link})", link_preview=".np" not in event.raw_text)
-
-    if not any(isinstance(x, (MessageEntityUrl, MessageEntityTextUrl)) for x in event.entities or []):
-        print(log)
-        await subreddit_reply()
-    elif re.search(r".(np)?f", event.raw_text):
-        print(log)
-        await subreddit_reply()
+### TESTING ###
+# Send as photo
+# @client.on(events.NewMessage)
+# async def lazy_download(event):
+#     # sender = await event.get_sender()
+#     # print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
+#     print(f"\n\n\nSTRINGIFY!\n\n\n{event.entities()}")
+#     if any(isinstance(x, (MessageMediaDocument)) for x in event. or []):
+#         event.reply(types.MessageMediaPhoto)
 
 
 client.start(bot_token=token)
