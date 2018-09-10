c1a0d4304146f082715722767922b5e2eb31c3b2
FrostySenpai <FrostySenpai@users.noreply.github.com>
2018-09-10 20:37:54 +0200

    Fixed the new line at line 12-13

--- plugins/convert_currency_plugin.py
+++ plugins/convert_currency_plugin.py
@@ -9,7 +9,6 @@ async def currency(event):
     sender = await event.get_sender()  # Get the sender
     result = c.convert(event.pattern_match.group(1), event.pattern_match.group(2), event.pattern_match.group(3))
     print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}: {result}")
-    await event.reply(
-        f"**{event.pattern_match.group(1)} {event.pattern_match.group(2)} is:**  `{result} {event.pattern_match.group(3)}`")
+    await event.reply(f"**{event.pattern_match.group(1)} {event.pattern_match.group(2)} is:**  `{result} {event.pattern_match.group(3)}`")
 
 currency.event = events.NewMessage(pattern=r"(^\d{1,9}|^\d{1,9}\.\d\d?) (\D{3}) to (\D{3})$")
