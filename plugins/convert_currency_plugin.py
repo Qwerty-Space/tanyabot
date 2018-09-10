5de793fd2fb7fe664031148ee0eeb2e32fd7cc53
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-10 20:10:28 +0100

    Update convert_currency_plugin.py

--- plugins/convert_currency_plugin.py
+++ plugins/convert_currency_plugin.py
@@ -1,8 +1,8 @@
 from telethon import events
 from currency_converter import CurrencyConverter
 
-c = CurrencyConverter()
 
+c = CurrencyConverter()
 
 # Convert Currency
 async def currency(event):
@@ -12,4 +12,3 @@ async def currency(event):
     await event.reply(f"**{event.pattern_match.group(1)} {event.pattern_match.group(2)} is:**  `{result} {event.pattern_match.group(3)}`")
 
 currency.event = events.NewMessage(pattern=r"(^\d{1,9}|^\d{1,9}\.\d\d?) (\D{3}) to (\D{3})$")
-
