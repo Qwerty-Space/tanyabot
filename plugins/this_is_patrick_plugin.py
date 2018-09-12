e58de0c358a91cabdd81b8b26d63e67d7be93b91
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-13 00:35:42 +0100

    Add files via upload

---
+++ plugins/this_is_patrick_plugin.py
@@ -0,0 +1,12 @@
+import re
+from telethon import events, sync
+from global_functions import probability
+
+
+# No, this is Patrick!
+async def this_is_patrick(event):
+    sender = await event.get_sender()
+    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
+    await event.reply("No, this is Patrick.")
+
+this_is_patrick.event = events.NewMessage(pattern=re.compile(r"^is this ((?:\S+ ?){1,4})\??$", re.I).search)
