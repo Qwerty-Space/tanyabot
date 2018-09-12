4e699f78e1096cb4d21c7c80b2a5421912474a39
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-13 00:46:31 +0100

    Added probability because people suck

--- plugins/this_is_patrick_plugin.py
+++ plugins/this_is_patrick_plugin.py
@@ -7,6 +7,7 @@ from global_functions import probability
 async def this_is_patrick(event):
     sender = await event.get_sender()
     print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
-    await event.reply("No, this is Patrick.")
+    if probability(0.2):
+        await event.reply("No, this is Patrick.")
 
 this_is_patrick.event = events.NewMessage(pattern=re.compile(r"^is this ((?:\S+ ?){1,4})\??$", re.I).search)
