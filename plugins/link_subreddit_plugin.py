9f3ebc0e8983eec73322b4991bcf9260501a1f78
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-13 14:47:07 +0100

    Handle the link in regex, and remove user

--- plugins/link_subreddit_plugin.py
+++ plugins/link_subreddit_plugin.py
@@ -7,29 +7,20 @@ from telethon.tl.types import MessageEntityTextUrl, MessageEntityUrl
 
 # Subreddit
 async def link_subreddit(event):
-    """Matches `r/subreddit` and `/r/subreddit`, and will return the subreddit as a link (ignores messages with links).
+    """Matches `r/subreddit` and `/r/subreddit`, and will return the subreddit as a link.
 **Arguments (positional):**
     • `.np`: - No preview
-    • `.f`: - Force match"""
+    """
     sender = await event.get_sender()
-    prefix = event.pattern_match.group(1)
-    subreddit_name = event.pattern_match.group(2)
-    suffix = event.pattern_match.group(3)
+    subreddit = event.pattern_match.group(1)
+    suffix = event.pattern_match.group(2)
     if not suffix:
         suffix = ""
-    subreddit_link = urljoin("https://reddit.com/", f"{prefix}/{subreddit_name}{suffix}")
-    log = f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}"
-
-    async def subreddit_reply():
-        await event.reply(f"[/{prefix}/{subreddit_name}{suffix}]({subreddit_link})", link_preview=".np" not in event.raw_text)
-
-    if not any(isinstance(x, (MessageEntityUrl, MessageEntityTextUrl)) for x in event.entities or []):
-        print(log)
-        await subreddit_reply()
-    elif re.search(r".(np)?f", event.raw_text):
-        print(log)
-        await subreddit_reply()
+    subreddit_link = urljoin("https://reddit.com/", f"{subreddit}{suffix}")
+   
+    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
+    await event.reply(f"[/{subreddit}{suffix}]({subreddit_link})", link_preview=".np" not in event.raw_text)
 
 link_subreddit.event = events.NewMessage(pattern=re.compile(
-                r"(?:\s|^)/?(r|u)/(\w+)(/(?:top|best|new|hot|rising|gilded|controversial|wiki(?:/\S+)?))?\b"
+                r"(?:[^/\w]|^|\s)/?(r/\w+)(/(?:top|best|new|hot|rising|gilded|controversial|wiki(?:/\S+)?))?\b"
                 ).search)
