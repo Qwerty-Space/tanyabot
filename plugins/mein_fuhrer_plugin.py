bc971e5b447fbfb69167293dde608c61031fd76a
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-09 00:29:59 +0100

    Added new sticker

--- plugins/mein_fuhrer_plugin.py
+++ plugins/mein_fuhrer_plugin.py
@@ -14,11 +14,13 @@ from .global_functions import probability
 @events.register(events.NewMessage(pattern=re.compile(r"\b(hitler|führer|fuhrer)\b", re.I).search, outgoing=False))
 async def mein_fuhrer(event):
     sender = await event.get_sender()
-    response_id = randint(0,2) # Roll for the response
+    response_id = randint(0,3) # Roll for the response
     print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}[{response_id}]: {event.pattern_match.string}")
     if response_id == 0:
         await event.reply("Heil Hitler!")
     elif response_id == 1:
         await event.reply("Sieg Heil!")
     elif response_id == 2:
-        await event.reply(file = "CAADAgADWgADraG3CP76-OQcP7msAg") # Sends a sticker
+        await event.reply(file="CAADAgADWgADraG3CP76-OQcP7msAg")
+    elif response_id == 3:
+        await event.reply(file="CAADBAADkQYAAhgwqgVYHov8PqiL9gI")
