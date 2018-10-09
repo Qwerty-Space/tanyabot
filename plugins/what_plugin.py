a53fd7253552313b72aaddbf22615a27b0676fe2
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-09 23:14:20 +0100

    Fixed logging

--- plugins/what_plugin.py
+++ plugins/what_plugin.py
@@ -9,11 +9,12 @@ from telethon import events, sync
 @events.register(events.NewMessage(pattern=r"(?i)^wh?[aou]t\??$"))
 async def wut(event):
     if event.is_reply:
-        sender = await event.get_sender()
-        print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
+        
         repliedmsg = await event.get_reply_message()
         await event.reply(f"**{repliedmsg.raw_text.upper()}**")
     else:
         prev_id = (event.id)-1
         prev_msg = await event.client.get_messages(event.chat_id, ids=prev_id)
         await event.reply(f"**{prev_msg.raw_text.upper()}**")
+    sender = await event.get_sender()
+    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
