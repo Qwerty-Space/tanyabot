94247522dce2f4abb5b29f0cb432f6bb28584fd2
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-18 22:19:35 +0100

    Update mein_fuhrer_plugin.py

--- plugins/mein_fuhrer_plugin.py
+++ plugins/mein_fuhrer_plugin.py
@@ -1,7 +1,7 @@
 r"""
 When a user mentions Hitler, or the Führer, the bot will respond with one of three messages.
 
-pattern:  `(?i)\b(hitler|führer|fuhrer)\b`
+pattern:  `(?i)\b(hitler|führer|fuhrer)\b` __(global)__
 """
 
 import re
@@ -11,7 +11,7 @@ from .global_functions import probability, log
 
 
 # MEIN FÜHRER!
-@events.register(events.NewMessage(pattern=r"(?i)\b(hitler|führer|fuhrer)\b", outgoing=False))
+@events.register(events.NewMessage(pattern=re.compile(r"(?i)\b(hitler|führer|fuhrer)\b").search, outgoing=False))
 async def mein_fuhrer(event):
     outcome = probability(0.4)
     response_id = randint(0,3) # Roll for the response
