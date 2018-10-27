42670a9603640c988f04dcdd7e0ebcd36768ea01
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-27 22:54:30 +0100

    Add files via upload

--- plugins/retaliate_plugin.py
+++ plugins/retaliate_plugin.py
@@ -1,7 +1,10 @@
 """If you say "love you botname/username" the bot will respond with "love you too!"
 It also works with other words, like "fuck", "screw", "damn"
 
-pattern:  `(?i)(love|fuck|screw|damn?) (yo)?u @?({fname}|{name}|{username})`
+patterns:  
+`(?i)((i ?)?l(ove)?( ?(y|(yo)?u))) @?({fname}|{name}|{username})`
+
+`(?i)(love|fuck|screw|damn?) (yo)?u @?({fname}|{name}|{username})`
 """
 
 import re
@@ -10,11 +13,12 @@ from .global_functions import log
 
 
 # Insult
-@events.register(events.NewMessage(pattern=r"(?i)(love|fuck|screw|damn?) (yo)?u @?(.+)", outgoing=False, forwards=False))
+@events.register(events.NewMessage(pattern=r"(?i)((i ?)?l(ove)?( ?(y|(yo)?u))) @?(?P<name>.+)", outgoing=False, forwards=False))
+@events.register(events.NewMessage(pattern=r"(?i)((fuck|screw|damn?) (yo)?u) @?(?P<name>.+)", outgoing=False, forwards=False))
 async def insult(event):
     name = (await event.client.get_me()).first_name
     fname = re.sub(r"\W.+", "", name)
     username = (await event.client.get_me()).username
-    if re.match(fr"(?i)({fname}|{name}|{username})", event.pattern_match.group(3)):
+    if re.match(fr"(?i)({fname}|{name}|{username})", event.pattern_match.group("name")):
         await log(event)
-        await event.reply(event.pattern_match.group(1)+" you too!")    # "Love you too!"
+        await event.reply(event.pattern_match.group(1)+" too!")    # "Love you too!"
