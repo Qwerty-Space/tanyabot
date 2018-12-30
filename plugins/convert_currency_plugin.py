17b68bd39672c949f747fe50c2be70c383ae5cd2
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-12-30 17:43:24 +0000

    Added /currencies command, and reduced accidental triggers

--- plugins/convert_currency_plugin.py
+++ plugins/convert_currency_plugin.py
@@ -1,21 +1,33 @@
-"""Converts two different currencies using the [European Central Bank's exchange rates](https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html).
+r"""Converts two different currencies using the [European Central Bank's exchange rates](https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html).
 
 Example: "GBP to/in USD" (case insensitive).
 You can also specify an amount of said currency:
 "5 GBP to USD".
 
-pattern:  `(?i)^(\d{1,9}|\d{1,9}\.\d\d?)? ?([a-z]{3}) (?:to|in) ([a-z]{3})$`
+List currencies using `/currencies`
+
+patterns:  
+`(?i)^(\d{1,9}|\d{1,9}\.\d\d?)? ?([a-z]{3}) (?:to|in) ([a-z]{3})$`
+
+`/currencies(@bots_username)?$`
 """
 
 from telethon import events
 from currency_converter import CurrencyConverter
 from .global_functions import log
+
 c = CurrencyConverter()
+link = "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html"
 
 
 # Convert Currency
 @events.register(events.NewMessage(pattern=r"(?i)^(\d{1,9}|\d{1,9}\.\d\d?)? ?([a-z]{3}) (?:to|in) ([a-z]{3})$"))
 async def currency(event):
+    stop_words = ["rip", "wut", "wot", "wat", "wet", "hec", "fak", "fuc", "fuk", "pep"]
+    for x in stop_words:
+        if x in event.pattern_match.string:
+            return
+
     fromval = event.pattern_match.group(1)
     if not fromval:
         fromval = 1
@@ -27,9 +39,17 @@ async def currency(event):
         await event.reply(f"**{fromval} {fromcur} is:**  `{result} {tocur}`")
     except ValueError:
         await log(event, "CURRENCY NOT AVAILABLE")
-        link = "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html"
         await event.reply(
-            f"**Sorry, that currency is not supported yet.**\nFor a list of supported currencies [click here.](%s)"
-            % (link),
+            f"**Sorry, that currency is not supported yet.**\nFor a list of supported currencies [click here]({link}) or send /currencies.",
             link_preview=False
         )
+
+@events.register(events.NewMessage(pattern=r"/currencies(@\w+)?$"))
+async def list_currencies(event):
+    usr_group = event.pattern_match.group(1)
+    username = (await event.client.get_me()).username
+    if usr_group and username not in usr_group:
+        return
+
+    text = f"**List of supported currencies:**\n {', '.join(sorted(c.currencies))}\n\nFor a detailed list of supported currencies [click here.]({link})"
+    await event.reply(text, link_preview=False)
