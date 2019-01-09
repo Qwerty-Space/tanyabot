815d05bcfb16b371c74a50513f631ddfe55f5c11
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2019-01-09 19:09:49 +0000

    Less creepy logging

--- plugins/global_functions.py
+++ plugins/global_functions.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from random import random
+import inspect
 import logging
 import asyncio
 import time
@@ -13,10 +14,11 @@ def probability(percent):
 # Logging
 async def log(event, info=""):
     sender = await event.get_sender()
-    try: 
-        message = event.pattern_match.string
-    except AttributeError:
-        message = ""
+    # try: 
+    #     message = event.pattern_match.string
+    # except AttributeError:
+    #     message = ""
+    message = inspect.currentframe().f_back.f_code.co_name
     logging.info(
         f"""[{event.date.strftime('%c')}]:
     [{sender.id}]@[{event.chat.id}] {sender.first_name}@{sender.username}: {message}
@@ -28,8 +30,10 @@ def cooldown(timeout):
         last_called = defaultdict(int)
 
         async def wrapped(event, *args, **kwargs):
+            print(event.chat_id, event.chat.id)
             current_time = time.time()
             if current_time - last_called[event.chat_id] < timeout:
+                print(last_called)
                 time_left = round(timeout - (current_time - last_called[event.chat_id]), 1)
                 await log(event, f"Cooldown: {time_left}s")
                 return
