67a68adf40164c9b4bdde9caf457ba540ffad89f
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2019-03-21 16:43:03 +0000

    Show image resolution when replying.  Provide feedback when processing

--- plugins/photo_plugin.py
+++ plugins/photo_plugin.py
@@ -1,5 +1,5 @@
 """When an image file is sent to the bot, it will respond with a compressed preview of the file.
-It will only respond to image files under 10 MB.
+It will only respond to image files under 15 MB (15000000 bytes).
 """
 
 import os
@@ -10,13 +10,14 @@ from .global_functions import log
 
 async def downscale(item):
     im = Image.open(item)
+    resolution = im.size
     size = 1280, 1280 # Rezise to a maxium of.  (Telegram's limit)
     outfile = f"{item}.thumb.{im.format}"
 
     im.thumbnail(size, Image.LANCZOS)
     im.save(outfile)
 
-    return outfile
+    return outfile, resolution
 
 
 @events.register(events.NewMessage(outgoing=False))
@@ -31,23 +32,29 @@ async def on_photo(event):
         return
     if "image" not in mime_type:
         return
-    if image.size > 15000000:
+    if image.size > 15000000: # 15 MB
         await event.reply("Image too large!  It must be under 15 MB.")
         await log(event, "Image too large!")
         return
+
+    notification = await event.reply("Loading...")
+    print(notification)
+    file_name = f"{msg.from_id}{image.id}{msg.id}.{mime_type[+6:]}"
+    await log(event, f"Image file: {file_name}")
+
     await event.client(functions.messages.SetTypingRequest(
         peer=event.from_id,
         action=types.SendMessageUploadPhotoAction(1)
     ))
 
-    file_name = f"{msg.from_id}{image.id}{msg.id}.{mime_type[+6:]}"
-    await log(event, f"Image file: {file_name}")
     await event.download_media(file=file_name)
-    im = await downscale(file_name)
+    im, resolution = await downscale(file_name)
+    dimensions = f"`{resolution[0]}x{resolution[1]}`"
     os.remove(file_name)
 
     try:
-        await event.reply(file=im)
+        await event.reply(message=dimensions, file=im)
     except errors.rpcerrorlist.PhotoInvalidDimensionsError:
-        await event.reply("The photo's dimensions are not supported by Telegram.")
+        await event.reply(f"The photo's dimensions ({dimensions}) are not supported by Telegram.")
+    await event.client.delete_messages(entity=None, message_ids=notification)
     os.remove(im)
