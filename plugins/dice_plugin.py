8605cd196bfcf4abe1a52e4f0d6825be84946629
cavemaneca <17803695+cavemaneca@users.noreply.github.com>
2019-09-03 09:50:12 -0600

    Update dice_plugin.py

--- plugins/dice_plugin.py
+++ plugins/dice_plugin.py
@@ -8,9 +8,10 @@ pattern: `/roll (?:(\d+)d|d)?(\d+)$`
 from random import randint
 from telethon import client, events
 from .global_functions import log
+import re
 
 
-@events.register(events.NewMessage(pattern=r"/roll(@\w+)? (?:(\d+)d|d)?(\d+)$"))
+@events.register(events.NewMessage(pattern=r"(?<=\/roll ).*(?:(?<= )(?:(\d+)d|d)?(\d+)){1,10}"))
 async def on_roll(event):
     usr_group = event.pattern_match.group(1)
     username = (await event.client.get_me()).username
@@ -18,29 +19,45 @@ async def on_roll(event):
         return
 
     m = event.pattern_match
-
-    if not m.group(2):
-        rolls = 1
-    else:
-        rolls = int(m.group(2))
-
-    sides = int(m.group(3))
-
-    if rolls > 500 or sides > 100000:
-        await event.respond("The maximum rolls is 500, and the maximum amount of sides is 100,000.")
-        await log(event, info="Bad roll")
+    
+    rollPattern = r"(?:(\d+)d|d)?(\d+){1,10}"
+    
+    matches = re.finditer(rollPattern, m)
+    
+    if not any(matches)
+        await log(event, info="Bad format")
         return
-
-    command = f"{rolls}d{sides}"
-
-    val = list()
+    
+    commands = list()
+    outputs = list()
     total = int()
-    for _ in range(0, rolls):
-        r = randint(1, sides)
-        val.append(str(r))
-        total += r
+    
+    for matchNum, match in enumerate(matches, start=1):
+            
+        if not match.group(2):
+            rolls = 1
+        else:
+            rolls = int(match.group(1))
 
-    output = " ".join(val)
+        sides = int(match.group(2))
+
+        if rolls > 500 or sides > 100000:
+            await event.respond("The maximum rolls is 500, and the maximum amount of sides is 100,000.")
+            await log(event, info="Bad roll")
+            return
+
+        commands.append(f"{rolls}d{sides}")
+
+        val = list()
+        for _ in range(0, rolls):
+            r = randint(1, sides)
+            val.append(str(r))
+            total += r
+
+        ouputs.append(" ".join(val))
+            
+    command.join(commands)
+    output = " ".join(outputs)
 
     await log(event)    # Logs the event
     await event.respond(f"**{command}:**\n`{output}`\n**=** `{total}`")
