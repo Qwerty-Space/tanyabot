6600cd2652b049da324dd53b51e289213a4c38b8
Qwerty-Space <oliver_duce@live.co.uk>
2020-05-08 02:35:49 +0100

    updated requirements and refactored m_f_plugin

--- plugins/mein_fuhrer_plugin.py
+++ plugins/mein_fuhrer_plugin.py
@@ -6,7 +6,7 @@ pattern:  `(?i)\b(hitler|f[uü]hrer)\b"` __(global)__
 """
 
 import re
-from random import randint
+from random import randint, choice
 from telethon import events, sync
 from .global_functions import log, cooldown
 
@@ -15,11 +15,9 @@ from .global_functions import log, cooldown
 @events.register(events.NewMessage(pattern=re.compile(r"(?i)\b(hitler|f[uü]hrer)\b").search, outgoing=False))
 @cooldown(60)
 async def mein_fuhrer(event):
-    response_id = randint(0,2) # Roll for the response
-    if response_id == 0:
-        await event.reply(file="CAADAgADWgADraG3CP76-OQcP7msAg")
-    elif response_id == 1:
-        await event.reply(file="CAADBAADkQYAAhgwqgVYHov8PqiL9gI")
-    elif response_id == 2:
-        await event.reply(file="CAADAgADRQADqh-tD2oBxZyI7uVhAg")
-    await log(event, response_id)
+    response_list = ["CAADAgADWgADraG3CP76-OQcP7msAg", "CAADBAADkQYAAhgwqgVYHov8PqiL9gI", "CAADAgADRQADqh-tD2oBxZyI7uVhAg"]
+    response_id = choice(response_list) # Roll for the response
+
+    await event.reply(file=response_id)
+
+    await log(event, response_list.index(response_id))
