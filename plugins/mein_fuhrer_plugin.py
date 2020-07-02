d481c43baceffa2600aee950b33b23e52e10aa2c
Qwerty-Space <oliver_duce@live.co.uk>
2020-07-02 23:35:05 +0100

    more triggers

--- plugins/mein_fuhrer_plugin.py
+++ plugins/mein_fuhrer_plugin.py
@@ -10,12 +10,17 @@ from random import randint, choice
 from telethon import events, sync
 from .global_functions import log, cooldown
 
+response_list = ["CAADAgADWgADraG3CP76-OQcP7msAg", # Tanya
+                 "CAADBAADkQYAAhgwqgVYHov8PqiL9gI", # The Professor
+                 "CAADAgADRQADqh-tD2oBxZyI7uVhAg" # Azunyan
+                ]
 
 # MEIN FÜHRER!
-@events.register(events.NewMessage(pattern=re.compile(r"(?i)\b(hitler|f[uü]hrer)\b").search, outgoing=False))
+@events.register(events.NewMessage(
+            pattern=re.compile(r"(?i)\b(hitler|f[uü]hrer|(14)?88|HH)\b").search,
+            outgoing=False))
 @cooldown(60)
 async def mein_fuhrer(event):
-    response_list = ["CAADAgADWgADraG3CP76-OQcP7msAg", "CAADBAADkQYAAhgwqgVYHov8PqiL9gI", "CAADAgADRQADqh-tD2oBxZyI7uVhAg"]
     response_id = choice(response_list) # Roll for the response
 
     await event.reply(file=response_id)
